86e93de1148e66b471c1e5421dddd634f659cbcb
Alan Xu <ayx002@ucsd.edu>
2017-12-17 21:37:36 -0800

    Fixed the position of links on each page, keep footer on the bottom

--- app/controllers/product_search_controller.rb
+++ app/controllers/product_search_controller.rb
@@ -3,12 +3,14 @@ class ProductSearchController < ApplicationController
   # GET /searches
   def index
     search = ProductSearchModel.where('query = ? AND pag_count = ?', params[:search], params[:pag].to_i).last
-    if search == nil
-      @items = Sem3SearchService.new(params).execute
-      Sem3CacheService.new.cache_query(params[:search], @items, params[:pag])
-    else
-      Rails.logger.info('+++ Retrieving from cache... +++')
-      @items = search.result
-    end
+    if params[:search] != nil
+      if search == nil
+        @items = Sem3SearchService.new(params).execute
+        Sem3CacheService.new.cache_query(params[:search], @items, params[:pag])
+      else
+        Rails.logger.info('+++ Retrieving from cache... +++')
+        @items = search.result
+      end
+    end 
   end
 end
